ef13c24c2f8eb0433918aacdecb7b37f2a8634f5
alfaris <muhamadalfariz2211@gmail.com>
2025-07-10 14:29:50 +0700

    update

--- src/components/home/contact-form.tsx
+++ src/components/home/contact-form.tsx
@@ -83,7 +83,7 @@ export function ContactForm() {
                 <motion.a href="https://www.instagram.com/alffars11" target="_blank" rel="noopener noreferrer" className="text-muted-foreground hover:text-primary transition-all p-4 bg-card/50 backdrop-blur-sm rounded-full border border-primary/20 hover:border-primary/50 shadow-lg hover:shadow-primary/20 transform hover:-translate-y-1 duration-300" variants={iconVariants}>
                     <Instagram className="h-10 w-10" />
                 </motion.a>
-                <motion.a href="https://facebook.com/VanessaRinn" target="_blank" rel="noopener noreferrer" className="text-muted-foreground hover:text-primary transition-all p-4 bg-card/50 backdrop-blur-sm rounded-full border border-primary/20 hover:border-primary/50 shadow-lg hover:shadow-primary/20 transform hover:-translate-y-1 duration-300" variants={iconVariants}>
+                <motion.a href="https://www.facebook.com/profile.php?id=61574923763690" target="_blank" rel="noopener noreferrer" className="text-muted-foreground hover:text-primary transition-all p-4 bg-card/50 backdrop-blur-sm rounded-full border border-primary/20 hover:border-primary/50 shadow-lg hover:shadow-primary/20 transform hover:-translate-y-1 duration-300" variants={iconVariants}>
                     <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="h-10 w-10">
                         <path d="M22.675 0h-21.35C.59 0 0 .59 0 1.325v21.35C0 23.41.59 24 1.325 24H12.82v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12V24h5.713c.735 0 1.325-.59 1.325-1.325V1.325C24 .59 23.41 0 22.675 0z"/>
                     </svg>
